1f67b18287cd718a660529ad212a8479c166287c
Ivan Mankovich <mankovich.ivan@gmail.com>
2020-09-29 09:35:40 +0300

    Updates

---
+++ precache-manifest.8d911d7329d629de32dc42df5cce20b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5b5a23d697d0ed70de5fc347e49982",
+    "url": "/SortingAlgorithms/index.html"
+  },
+  {
+    "revision": "50636f336b606323c642",
+    "url": "/SortingAlgorithms/static/css/main.98094482.chunk.css"
+  },
+  {
+    "revision": "d41d4b9efc057ea42e9f",
+    "url": "/SortingAlgorithms/static/js/2.90559b05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SortingAlgorithms/static/js/2.90559b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50636f336b606323c642",
+    "url": "/SortingAlgorithms/static/js/main.163f119a.chunk.js"
+  },
+  {
+    "revision": "5f8cdeed071d9344c58c",
+    "url": "/SortingAlgorithms/static/js/runtime-main.148b4a5e.js"
+  }
+]);
